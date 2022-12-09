b217fa660f8fb455879e69f536e185ce8080eda6
Lolillo <lmanuel.garciam@gmail.com>
2022-12-09 15:32:21 +0100

    BasketOptionVectorized function added

--- Option_formulas.py
+++ Option_formulas.py
@@ -36,3 +36,36 @@ def BlackTF(Forward, Strike, TTM, rate, Vol, IsCall):
     else:
       return tf.maximum(-Forward+Strike,0)
 
+
+def BasketOptionVectorized(num_assets, initial_prices, strike_prices,
+                  gross_return_strike,
+                  repo_rates, discount_rate, dividends, 
+                  volatilities, correlations,
+                  time_to_maturity, IsCall):
+  '''
+   Inputs:
+  -------
+    num_assets: Number of underlying assets
+    initial_prices: Prices of the basket constituents as of value date
+    strike_prices: Prices in the denominator of the prod function
+    gross_return_strike: gross return strike
+    repo_rates: Array of repo rates
+    discount_rate: Discount rate
+    dividends: Array of dividend yields
+    volatilities: Array of volatilities
+    correlations: Matrix of instantaneous correlations
+    time_to_maturity: Time to maturity
+  Output:
+    tf.Tensor: Option premium
+  '''
+
+  A = tf.pow(tf.reduce_prod(initial_prices/strike_prices, axis = 1), 1.0/num_assets)
+
+  mu = tf.reduce_mean(repo_rates - dividends-0.5*volatilities*volatilities, axis = 1)*time_to_maturity
+
+  Sigma = tf.sqrt(time_to_maturity * tf.reduce_sum(volatilities* 
+                  tf.matmul(volatilities, correlations), axis = 1)) / num_assets
+                  
+  return BlackTF(A*tf.exp(mu+0.5*Sigma*Sigma), gross_return_strike, time_to_maturity, 
+                 discount_rate, Sigma/tf.sqrt(time_to_maturity),IsCall)   
+
