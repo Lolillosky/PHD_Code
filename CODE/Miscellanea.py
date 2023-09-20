7a2cf98445068e508a8e42cb71ffa194c9a9de79
Lolillo <lmanuel.garciam@gmail.com>
2023-09-20 17:30:54 +0200

    Fucntions included in Miscellanea.py
    concat_dict_containing_np_arrays: to contatenate arrays contained in dictionaries with same keys.
    shuffle_arrays_in_dict: To shuffle arrays with same number of rows included in same dictionary.

--- CODE/Miscellanea.py
+++ CODE/Miscellanea.py
@@ -59,5 +59,92 @@ def plot_schocks(number_risk_factors, list_of_shocks, risk_factor_names, bins):
                 ax[i,j].set_xlabel(risk_factor_names[j])
 
 
-    f.set_size_inches(15,15)       
+    f.set_size_inches(15,15)     
+
+
+
+def concat_dict_containing_np_arrays(dict_list, axis=0):
+    """
+    Concatenate numpy arrays from a list of dictionaries along a specified axis.
+    
+    Parameters:
+    - dict_list (list of dict): A list of dictionaries where each dictionary has the same keys 
+                                and each key corresponds to a numpy array.
+    - axis (int, optional): The axis along which the arrays will be concatenated. Default is 0.
+    
+    Returns:
+    - dict: A dictionary with the same keys as the input dictionaries. Each key corresponds to 
+            the concatenated numpy array from all dictionaries in the list.
+    
+    Raises:
+    - ValueError: If dictionaries don't have the same keys or arrays can't be concatenated.
+    """
+
+    ret_dict = {}
+
+    if not all(set(d.keys()) == set(dict_list[0].keys()) for d in dict_list):
+        raise ValueError("All dictionaries in dict_list must have the same keys.")
+
+    # Loop through the keys in the first dictionary (assuming all dictionaries have the same keys)
+    for key in dict_list[0].keys():
+        try:
+            # For each key, retrieve the corresponding numpy arrays from each dictionary in the list
+            list_of_arrays = [x[key] for x in dict_list]
+
+            # Concatenate the arrays along the specified axis and store in the return dictionary
+            ret_dict[key] = np.concatenate(list_of_arrays, axis=axis)
+        
+        except ValueError as e:
+            raise ValueError(f"Error concatenating arrays for key '{key}': {str(e)}")
+
+    return ret_dict
+
+import numpy as np
+from sklearn.utils import shuffle
+
+def shuffle_arrays_in_dict(my_dict, random_state=None):
+    """
+    Shuffle arrays in the dictionary based on a common index.
+    
+    Parameters:
+    - my_dict (dict): Dictionary containing arrays to be shuffled.
+    - random_state (int, optional): Seed for reproducibility.
+    
+    Returns:
+    - dict: Dictionary with shuffled arrays.
+    
+    Raises:
+    - ValueError: If arrays within the dictionary have mismatched lengths.
+    """
+    
+    # Check if dictionary is not empty
+    if not my_dict:
+        raise ValueError("The dictionary is empty.")
+    
+    # Determine the length of the first array in the dictionary for common shuffling
+    first_key = list(my_dict.keys())[0]
+    length = my_dict[first_key].shape[0]
+    
+    # Verify that all arrays in the dictionary have the same length
+    for key, value in my_dict.items():
+        if value.shape[0] != length:
+            raise ValueError(f"All arrays in the dictionary must have the same length. Array '{key}' has a mismatched length.")
+    
+    # Generate a shuffled index based on the length of the arrays
+    shuffled_index = shuffle(np.arange(length), random_state=random_state)
+    
+    # Create a copy of the dictionary and shuffle arrays based on the common index
+    my_dict_copy = {}
+    for key, value in my_dict.items():
+        my_dict_copy[key] = value[shuffled_index]
+        
+    return my_dict_copy
+
+    # Example usage:
+    # data = {'a': np.array([1, 2, 3]), 'b': np.array([4, 5, 6])}
+    # shuffled_data = shuffle_arrays_in_dict(data)
+    # print(shuffled_data)
+
+
+
 
